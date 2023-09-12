f53ccc49ce3e69da011dfc5b87525d6e603f002e
meshtag <prathameshtagore@gmail.com>
2023-09-12 16:44:12 +0200

    Add comments in lru list example

--- examples/lru_implementation.cpp
+++ examples/lru_implementation.cpp
@@ -41,13 +41,17 @@ class DCDS_Node {
   }
 
   static void initialize() {
+    // context(enable_multithreading_flag, allow_dangling_functions_flag)
     dcds::DCDSContext context(false, false);
+    // builder(DCDSContext, data_structure_name)
     dcds::Builder dsBuilder(context, "Node");
 
+    // addAttribute(attribute_name, attribute_type, initial_value)
     dsBuilder.addAttribute("payload", dcds::INTEGER, 0);
     dsBuilder.addAttribute("next_reference", dcds::RECORD_PTR, nullptr);
     dsBuilder.addAttribute("prev_reference", dcds::RECORD_PTR, nullptr);
 
+    // createFunction(function_name, function_return_type, <optional>function_arguments_types)
     auto read_payload_fn = dsBuilder.createFunction("read_payload_function", dcds::valueType::INTEGER);
     auto read_next_ref_fn = dsBuilder.createFunction("read_next_ref_function", dcds::valueType::RECORD_PTR);
     auto read_prev_ref_fn = dsBuilder.createFunction("read_prev_ref_function", dcds::valueType::RECORD_PTR);
@@ -55,14 +59,17 @@ class DCDS_Node {
     auto write_fn = dsBuilder.createFunction("write_function", dcds::valueType::VOID, dcds::valueType::INTEGER,
                                              dcds::valueType::RECORD_PTR, dcds::valueType::RECORD_PTR);
 
+    // addTempVar(temp_var_name, temp_var_type, temp_var_initial_value, function_to_which_it_is_attached)
     dsBuilder.addTempVar("node_read_payload_variable", dcds::valueType::INTEGER, 0, read_payload_fn);
     dsBuilder.addTempVar("node_read_next_ref_variable", dcds::valueType::RECORD_PTR, nullptr, read_next_ref_fn);
     dsBuilder.addTempVar("node_read_prev_ref_variable", dcds::valueType::RECORD_PTR, nullptr, read_prev_ref_fn);
 
+    // addArgVar(arg_var_name, arg_var_type, function_to_which_it_is_attached)
     dsBuilder.addArgVar("node_write_payload_variable", dcds::valueType::INTEGER, write_fn);
     dsBuilder.addArgVar("node_write_next_ref_variable", dcds::valueType::RECORD_PTR, write_fn);
     dsBuilder.addArgVar("node_write_prev_ref_variable", dcds::valueType::RECORD_PTR, write_fn);
 
+    // createReadStatement(attribute_to_be_read_from, temp_var_name_which_will_have_read_value)
     auto read_payload_statement =
         dsBuilder.createReadStatement(dsBuilder.getAttribute("payload"), "node_read_payload_variable");
     auto read_next_ref_statement =
@@ -70,6 +77,7 @@ class DCDS_Node {
     auto read_prev_ref_statement =
         dsBuilder.createReadStatement(dsBuilder.getAttribute("prev_reference"), "node_read_prev_ref_variable");
 
+    // createUpdateStatement(attribute_to_be_written_to, temp_var_which_will_have_value_to_write)
     auto write_payload_statement =
         dsBuilder.createUpdateStatement(dsBuilder.getAttribute("payload"), "node_write_payload_variable");
     auto write_next_ref_statement =
@@ -77,10 +85,12 @@ class DCDS_Node {
     auto write_prev_ref_statement =
         dsBuilder.createUpdateStatement(dsBuilder.getAttribute("prev_reference"), "node_write_prev_ref_variable");
 
+    // createReturnStatement(temp_var_name_which_is_to_be_returned)
     auto return_payload_statement = dsBuilder.createReturnStatement("node_read_payload_variable");
     auto return_next_ref_statement = dsBuilder.createReturnStatement("node_read_next_ref_variable");
     auto return_prev_ref_statement = dsBuilder.createReturnStatement("node_read_prev_ref_variable");
 
+    // addStatement(statement, function_to_which_it_should_be_added)
     dsBuilder.addStatement(read_payload_statement, read_payload_fn);
     dsBuilder.addStatement(return_payload_statement, read_payload_fn);
 
@@ -94,12 +104,14 @@ class DCDS_Node {
     dsBuilder.addStatement(write_next_ref_statement, write_fn);
     dsBuilder.addStatement(write_prev_ref_statement, write_fn);
 
+    // addFunction(function_to_be_added)
     dsBuilder.addFunction(read_payload_fn);
     dsBuilder.addFunction(read_next_ref_fn);
     dsBuilder.addFunction(read_prev_ref_fn);
 
     dsBuilder.addFunction(write_fn);
 
+    // Generate code
     visitor = dsBuilder.codegen();
 
     built_read_payload_fn = visitor->getBuiltFunctionInt64ReturnType("read_payload_function");
@@ -182,7 +194,7 @@ class DCDS_DL_LinkedList {
     DCDS_Node *ref_ptr = front;
 
     for (int64_t i = 0; i < size; ++i) {
-       LOG(INFO) << ref_ptr->read_payload();
+      LOG(INFO) << ref_ptr->read_payload();
       ref_ptr = reinterpret_cast<DCDS_Node *>(ref_ptr->read_prev_ref());
     }
   }
@@ -260,15 +272,6 @@ static void check1(DCDS_LRU_List *dli) {
   dli->refer(1);
   dli->refer(4);
   dli->refer(5);
-}
-
-static void check2(DCDS_LRU_List *dli) {
-  dli->refer(11);
-  dli->refer(22);
-  dli->refer(33);
-  dli->refer(11);
-  dli->refer(44);
-  dli->refer(55);
 }
 
 int main() {
