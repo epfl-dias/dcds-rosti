b69312aea42495e54ec36266754062b079f94d55
Hamish Nicholson <hmnicholson12@gmail.com>
2023-10-09 20:53:12 +0200

    Fix cache variable for tbb config

--- cmake/tbb.cmake
+++ cmake/tbb.cmake
@@ -1,3 +1,4 @@
-SET(TBB_TEST OFF)
-SET(TBB_STRICT OFF)
+# cache is neccessary because tbb uses cmake minimumun version 3.1 and does not set CMP0077
+SET(TBB_TEST OFF CACHE BOOL "build tbb tests")
+SET(TBB_STRICT OFF CACHE BOOL "warnings as errors for tbb")
 include(external/CMakeLists.txt.tbb.in)
