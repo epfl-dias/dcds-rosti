adbdb7b8edbec0c8ab5c7ccafe58339b4f93ca54
Aunn Raza <aunnraza@hotmail.com>
2023-10-13 23:31:18 +0200

    Add skeleton for expressions

---
+++ dcds/include/dcds/builder/expressions/unary-expressions.hpp
@@ -0,0 +1,100 @@
+/*
+                              Copyright (c) 2023.
+          Data Intensive Applications and Systems Laboratory (DIAS)
+                  École Polytechnique Fédérale de Lausanne
+
+                              All Rights Reserved.
+
+      Permission to use, copy, modify and distribute this software and
+      its documentation is hereby granted, provided that both the
+      copyright notice and this permission notice appear in all copies of
+      the software, derivative works or modified versions, and any
+      portions thereof, and that both notices appear in supporting
+      documentation.
+
+      This code is distributed in the hope that it will be useful, but
+      WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. THE AUTHORS
+      DISCLAIM ANY LIABILITY OF ANY KIND FOR ANY DAMAGES WHATSOEVER
+      RESULTING FROM THE USE OF THIS SOFTWARE.
+ */
+
+#ifndef DCDS_UNARY_EXPRESSIONS_HPP
+#define DCDS_UNARY_EXPRESSIONS_HPP
+
+#include <any>
+#include <iostream>
+#include <utility>
+
+#include "dcds/builder/expressions/expressions.hpp"
+#include "dcds/common/types.hpp"
+#include "dcds/util/logging.hpp"
+
+namespace dcds::expressions {
+
+class UnaryExpression : public Expression {
+ public:
+  UnaryExpression() = default;
+  [[nodiscard]] int getNumOperands() const final { return 1; }
+  [[nodiscard]] bool isUnaryExpression() const final { return true; }
+  [[nodiscard]] bool isBinaryExpression() const final { return false; }
+};
+
+class IsNullExpression : public UnaryExpression {
+ public:
+  explicit IsNullExpression(Expression* expr) : expr(expr) {}
+  explicit IsNullExpression(const std::shared_ptr<Expression>& expr) : expr(expr.get()) {}
+
+  [[nodiscard]] valueType getResultType() const override { return valueType::BOOL; }
+
+  Expression* getExpression() { return expr; }
+
+ private:
+  Expression* expr;
+};
+
+class LocalVariableExpression : public UnaryExpression {
+ protected:
+  explicit LocalVariableExpression(std::string variable_name, dcds::valueType variable_type,
+                                   VAR_SOURCE_TYPE source_type)
+      : var_name(std::move(variable_name)), var_type(variable_type), var_src_type(source_type) {}
+
+ public:
+  [[nodiscard]] auto getName() { return var_name; };
+  [[nodiscard]] auto getType() const { return var_type; }
+  [[nodiscard]] auto getSourceType() const { return var_src_type; }
+
+  [[nodiscard]] valueType getResultType() const override { return var_type; }
+
+ public:
+  const std::string var_name;
+  const dcds::valueType var_type;
+  const VAR_SOURCE_TYPE var_src_type;
+};
+
+class TemporaryVariableExpression : public LocalVariableExpression {
+ public:
+  explicit TemporaryVariableExpression(std::string variable_name, dcds::valueType variable_type, std::any default_value)
+      : LocalVariableExpression(std::move(variable_name), variable_type, VAR_SOURCE_TYPE::TEMPORARY_VARIABLE),
+        var_default_value(std::move(default_value)) {}
+
+  explicit TemporaryVariableExpression(std::string variable_name, dcds::valueType variable_type)
+      : TemporaryVariableExpression(std::move(variable_name), variable_type, {}) {}
+
+  ~TemporaryVariableExpression() override { LOG(INFO) << "~TemporaryVariableExpression"; }
+
+ public:
+  const std::any var_default_value;
+};
+
+class FunctionArgumentExpression : public LocalVariableExpression {
+ public:
+  explicit FunctionArgumentExpression(std::string variable_name, dcds::valueType variable_type)
+      : LocalVariableExpression(std::move(variable_name), variable_type, VAR_SOURCE_TYPE::FUNCTION_ARGUMENT) {}
+
+  ~FunctionArgumentExpression() override { LOG(INFO) << "~FunctionArgumentExpression"; }
+};
+
+}  // namespace dcds::expressions
+
+#endif  // DCDS_UNARY_EXPRESSIONS_HPP
---
+++ dcds/include/dcds/codegen/llvm-codegen/expression-codegen/llvm-expression-visitor.hpp
@@ -0,0 +1,45 @@
+/*
+                              Copyright (c) 2023.
+          Data Intensive Applications and Systems Laboratory (DIAS)
+                  École Polytechnique Fédérale de Lausanne
+
+                              All Rights Reserved.
+
+      Permission to use, copy, modify and distribute this software and
+      its documentation is hereby granted, provided that both the
+      copyright notice and this permission notice appear in all copies of
+      the software, derivative works or modified versions, and any
+      portions thereof, and that both notices appear in supporting
+      documentation.
+
+      This code is distributed in the hope that it will be useful, but
+      WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. THE AUTHORS
+      DISCLAIM ANY LIABILITY OF ANY KIND FOR ANY DAMAGES WHATSOEVER
+      RESULTING FROM THE USE OF THIS SOFTWARE.
+ */
+
+#ifndef DCDS_LLVM_EXPRESSION_VISITOR_HPP
+#define DCDS_LLVM_EXPRESSION_VISITOR_HPP
+
+#include "dcds/builder/expressions/expression-visitor.hpp"
+#include "dcds/builder/expressions/expressions.hpp"
+#include "dcds/codegen/llvm-codegen/llvm-codegen.hpp"
+
+namespace dcds {
+
+class LLVMExpressionVisitor : public ExpressionVisitor {
+ public:
+  explicit LLVMExpressionVisitor(LLVMCodegen* codegen_engine) : codegenEngine(codegen_engine) {}
+
+ public:
+  void visit(const expressions::IsNullExpression& isNull) override;
+  void visit(const expressions::AddExpression& isNull) override;
+
+ private:
+  LLVMCodegen* codegenEngine;
+};
+
+}  // namespace dcds
+
+#endif  // DCDS_LLVM_EXPRESSION_VISITOR_HPP
