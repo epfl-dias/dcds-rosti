3baa74b7ff8931b93d8bbd91d8d97fe57e581432
Hamish Nicholson <hmnicholson12@gmail.com>
2023-10-19 13:57:47 +0200

    [build] tweak abseil to not emit warnings

--- cmake/abseil.cmake
+++ cmake/abseil.cmake
@@ -1,10 +1,7 @@
 set(ABSL_BUILD_TESTING OFF CACHE BOOL "" FORCE)
 set(ABSL_USE_EXTERNAL_GOOGLETEST OFF CACHE BOOL "" FORCE)
 set(ABSL_PROPAGATE_CXX_STD ON)
+set(ABSL_USE_SYSTEM_INCLUDES ON)
+set(ABSL_BUILD_TEST_HELPERS OFF)
 include(external/CMakeLists.txt.abseil.in)
-
-# Make glog appear as system library to avoid header warnings
-#get_target_property(glog_INCLUDE_DIR glog INTERFACE_INCLUDE_DIRECTORIES)
-#target_include_directories(glog SYSTEM PUBLIC ${glog_INCLUDE_DIR})
-#add_library(glog::glog ALIAS glog)
 
